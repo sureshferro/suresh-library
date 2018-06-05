d1f9b532a22ee1b481a5a822919279bccfba18ad
sureshferro <39941385+sureshferro@users.noreply.github.com>
2018-06-05 00:39:01 -0700

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,13 @@
+# suresh-library
+a=int(input("enter the value"))
+if(a<=100000):
+    if(a<0):
+        print("\n negative")
+    elif(a>0):
+        print("\n positive")
+    elif(a==0):
+        print("\n zero")
+    else:
+        ("correct number")
+else:
+     print("the number is above 100000 invalid")
