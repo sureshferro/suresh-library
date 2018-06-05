f7c507d0f5c22067fa5192fd42395f090f755e2f
sureshferro <39941385+sureshferro@users.noreply.github.com>
2018-06-05 02:37:32 -0700

    Create LARGEST.py

---
+++ LARGEST.py
@@ -0,0 +1,18 @@
+# suresh-library
+a=int(input("enter value 1"))
+b=int(input("enter value 2"))
+c=int(input("enter value 3"))
+if(a>b and a>c):
+	print("\n a is large")
+else: 
+	print("\n a is small") 
+if(b>a and b>c):
+	print("b is large") 
+else: 
+	print("b is small") 
+if(c>a and c>b):
+	print("c is large") 
+else: 
+            print("c is small")
+ 
+    
