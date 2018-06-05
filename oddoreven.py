42a25d2ee363393720564446ab86afe51dd81f83
sureshferro <39941385+sureshferro@users.noreply.github.com>
2018-06-05 00:53:15 -0700

    Update and rename positive.py to oddoreven.py

---
+++ oddoreven.py
@@ -0,0 +1,9 @@
+# suresh-library
+a=int(input("enter the value"))
+if(a<=100000):
+    if(a%2==0):
+        print("\n even")
+    else:
+        print("\n odd")
+else:
+    print("the number is above 100000 invalid")
