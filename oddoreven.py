e6441768fb450d8c46fae6fc406273ad369fcfc9
sureshferro <39941385+sureshferro@users.noreply.github.com>
2018-06-05 01:41:06 -0700

    Update and rename oddoreven.py to vowel.py

--- oddoreven.py
+++
@@ -1,9 +0,0 @@
-# suresh-library
-a=int(input("enter the value"))
-if(a<=100000):
-    if(a%2==0):
-        print("\n even")
-    else:
-        print("\n odd")
-else:
-    print("the number is above 100000 invalid")
---
+++ vowel.py
@@ -0,0 +1,12 @@
+# suresh-library
+s =("a","e","i","o","u"
+)
+z = input("")
+if z in s: 
+    print("vowel")
+else:
+    print("consonent")
+    	
+    
+    
+   
