87c1536253f18ff46dfbfe99690db2a0a2163550
sureshferro <39941385+sureshferro@users.noreply.github.com>
2018-06-05 03:35:32 -0700

    Create array.py

---
+++ array.py
@@ -0,0 +1,13 @@
+# suresh-library
+n=int(input())
+y=[]
+sum=0
+for i in range(0,n):
+	b=int(input(""))
+	y.append(b)
+print(y)
+k=int(input())
+sum=0
+for j in range(0,k):
+	sum=sum+y[j]
+print(sum)
